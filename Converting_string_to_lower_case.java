0ad0ced7b2cde3f56db525a622fa3e591bfc6663
Kamparapu Lokesh Pavan Teja <lokeshpavantejakamparapu@gmail.com>
2026-03-20 13:52:14 +0530

    Add files via upload

---
+++ Converting_string_to_lower_case.java
@@ -0,0 +1,10 @@
+class Solution {
+    static String toLower(String s) {
+        s.toLowerCase();
+        return s.toLowerCase(); /*
+                                 * s.toLowerCase is used to convert
+                                 * the uppercase letter
+                                 * into the lower case
+                                 */
+    }
+}
