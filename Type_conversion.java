4a0ee22bba78258a78ea046261118fa4b3c53b87
Kamparapu Lokesh Pavan Teja <lokeshpavantejakamparapu@gmail.com>
2026-03-22 16:29:29 +0530

    Add files via upload

---
+++ Type_conversion.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Type_conversion {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+        System.out.println((int) d);
+    }
+
+}
