34060b59e8766fe3af04cdbb4685c7fe28e6f561
Kamparapu Lokesh Pavan Teja <lokeshpavantejakamparapu@gmail.com>
2026-03-23 20:29:49 +0530

    Add files via upload

---
+++ Type_casting_and_double_it.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Type_casting_and_double_it {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String num = sc.nextLine();
+        int n = Integer.parseInt(num);
+        int result = n * 2;
+        System.out.print(result);
+    }
+}
